a7992ab74af80af5f87c486f1f26cf72a0db6ffb
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-23 15:31:41 +0500

    feat: wrap App component with React.StrictMode and BrowserRouter for improved structure

--- my-protfilo/src/main.jsx
+++ my-protfilo/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { TooltipProvider } from "@/components/ui/tooltip"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <TooltipProvider>
-    <App />
-  </TooltipProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
